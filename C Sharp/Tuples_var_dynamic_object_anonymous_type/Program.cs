44d40fb0f7f91df213e12d70a9cdf15acfd4a309
Karol Dubas <krizz.kontakt@gmail.com>
2022-11-18 16:52:02 +0100

    Add Tuples_var_dynamic_object_anonymous_type

---
+++ C Sharp/Tuples_var_dynamic_object_anonymous_type/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tuples;
+
+namespace Tuples_var_dynamic_object_anonymous_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Tuples.Tuples();
+            new AnonymousType();
+            new FlexTypes();
+        }
+    }
+}
