9b06953e6842bdab40bde71a42f8168ac3485e0a
Karol Dubas <krizz.kontakt@gmail.com>
2022-11-18 14:50:04 +0100

    Add Attributes

---
+++ C Sharp/Attributes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Attributes
+{
+    [Help("class")]
+    class Program
+    {
+        // Tylko na metody i klasy
+        //[Help("test")]
+        public int Property { get; set; }
+        
+        [Help("method")]
+        static void Main() { }
+    }
+
+    // Jak property nie ma w konstruktorze atrybutu, to trzeba ręcznie wpisać
+    // np. jak tu Message = "..."
+    [DebugInfo(1, "Karol", "03-03-2021", Message = "Jakiś błąd")]
+    class TestClass
+    {
+    }
+}
