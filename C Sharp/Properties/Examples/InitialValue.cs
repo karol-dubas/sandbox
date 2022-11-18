199320595be677375c7c059c92f9576caa28ea38
Karol Dubas <krizz.kontakt@gmail.com>
2022-11-18 13:14:13 +0100

    Add Properties

---
+++ C Sharp/Properties/Examples/InitialValue.cs
@@ -0,0 +1,23 @@
+﻿namespace Properties
+{
+    public class InitialValue
+    {
+        public int Width { get; set; }
+        public int Length { get; set; } = 1;
+
+        private int _age = 1;
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        
+        public InitialValue()
+        {
+            Width = 1;
+
+            // Kontruktor nadpisuje init property
+            Length = 2;
+        }
+    }
+}
---
+++ C Sharp/Properties/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main()
+        {
+            new FullPropertyVsAutoProperty();
+            new PropertyVsPublicField();
+            new InitialValue();
+            new ReadonlyProperty();
+            new PropertyAccessors();
+        }
+    }
+}
