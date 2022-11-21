f2647ebe0d8906a1f0f42560e8a3f0c8d657b996
Karol Dubas <krizz.kontakt@gmail.com>
2022-11-21 19:09:54 +0100

    Add JSON

---
+++ Libraries/JSON/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSON
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public decimal Price { get; set; }
+        public string[] Sizes { get; set; }
+    }
+}
