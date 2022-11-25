76bf5a63937b0bc986464ca852a49a404603666b
Karol Dubas <krizz.kontakt@gmail.com>
2022-11-25 23:12:19 +0100

    Add WPF

---
+++ WPF/MVVM_demo/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace MVVM_demo
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ WPF/WPF_examples/Size.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_examples
+{
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large,
+        Enormous
+    }
+}
