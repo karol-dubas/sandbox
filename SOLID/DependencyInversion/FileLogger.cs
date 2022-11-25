aab80f0226b0f3457eb12b315dc865581bdb0bea
Karol Dubas <krizz.kontakt@gmail.com>
2022-11-25 19:38:54 +0100

    Add SOLID - DependencyInversion

---
+++ SOLID/DependencyInversion/FileLogger.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace DependencyInversion
+{
+	public class FileLogger : ILogger
+	{
+		private readonly TextWriter _writer;
+
+		public FileLogger(TextWriter writer)
+		{
+			_writer = writer;
+		}
+
+		public void Log(string message)
+		{
+			_writer.Write(message);
+		}
+	}
+}
---
+++ SOLID/DependencyInversion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace DependencyInversion
+{
+	internal class Program
+	{
+		// Moduł wysokopoziomowy - UserManager
+		// Moduły niskopoziomowe - implementacje ILogger 
+		
+		// Inwersja zależności służy do łączenia modułów oprogramowania, wszystkie zależności
+		// wysokopoziomowych i niskopoziomowych modułów zostają odwrócone.
+
+		// Moduły wysokiego poziomu nie powinny zależeć od modułów niskiego poziomu,
+		// nie powinny być zależne od szczegółów implementacji modułów niskopoziomowych.
+		
+		// Moduły niskiego i wysokiego poziomu powinny zależeć od abstrakcji.
+
+		private static void Main(string[] args)
+		{
+			var sw = GetWriter();
+			var userManager = new UserManager(new FileLogger(sw));
+			userManager.Login("test@test.com", "password");
+		}
+
+		private static StreamWriter GetWriter()
+		{
+			string path = @$"{Environment.CurrentDirectory}\logs.txt";
+			using var sw = new StreamWriter(path);
+			return sw;
+		}
+	}
+}
